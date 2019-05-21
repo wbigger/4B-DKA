f55263fb3b03f299d72e3d4e81e71924e420f542
wbigger <wbigger@gmail.com>
2019-05-21 09:04:29 +0200

    Add synchronized keyword

--- src/marconi/c4binf/orale/SimpleThread.java
+++ src/marconi/c4binf/orale/SimpleThread.java
@@ -3,28 +3,57 @@ package marconi.c4binf.orale;
 import java.util.Date;
 
 public class SimpleThread {
-	static class LoadVideoThread implements Runnable{
-
-		
-		public void run() {
+	
+	static class ProgressBar {
+		// this method should NOT be interrupted by other threads
+		// to do this, we use the keyword "synchronized"
+		synchronized void show() {
 			for(int i=0;i<100;i++){
-				//double f = 100.0/1000.0*0.9999999+1.01*0.99999*0.11111;
 				Date date = new Date();
 				System.out.println("Thread "+ Thread.currentThread().getName()+" is loading "+i*1+"% @"+date);
 			}
-			
+		}
+	}
+	
+	static class LoadVideoThread implements Runnable{
+		ProgressBar progressBar;
+		
+		public LoadVideoThread(ProgressBar progressBar) {
+			super();
+			this.progressBar = progressBar;
+		}
+		
+		public void run() {
+			// use the global instance of the progress bar
+			progressBar.show();
 		}
 		
 	}
 
 	public static void main(String[] args) {
-		Thread t1=new Thread(new LoadVideoThread(),"DKA");
+		// Our application has a single, global progress bar
+		// on the top of the screen
+		ProgressBar pb = new ProgressBar();
+		
+		// pass the global progress bar as argument to the new threads
+		Thread t1=new Thread(new LoadVideoThread(pb),"DKA");
 		t1.setPriority(1);
-		Thread t2=new Thread(new LoadVideoThread(),"DKA2");
+		Thread t2=new Thread(new LoadVideoThread(pb),"DKA2");
 		t2.setPriority(10);
         
+		// launch the threads
 		t1.start();
         t2.start();
+        
+        // as we can see running this program, the DKA progress is
+        // _always_ executed first, whatever is its priority
+        // this is because the block of code inside synchronized
+        // cannot be interrupted!
+        
+        // NOTE: another way to synchronize block of code is as follow:
+        // synchronized (this) {
+        //    ...some code here...
+        // }
 	}
 
 }
